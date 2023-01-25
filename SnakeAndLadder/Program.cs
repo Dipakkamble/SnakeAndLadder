1c11351e4174eff79b50ed87559d69d05e8d66bc
Dipakkamble <kambledipak700@gmail.com>
2023-01-25 18:51:35 +0530

    [Dipak] modified start position of player

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -10,7 +10,9 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            int startPosition = 0;
             Console.WriteLine("welcome to snake and ladder program");
+            Console.WriteLine("single player start position : " +startPosition);
             Console.ReadLine();
         }
     }
