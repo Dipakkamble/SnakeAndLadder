4361fc2051da3e79911ec4035b335a765d3f6319
Dipakkamble <kambledipak700@gmail.com>
2023-01-27 16:42:16 +0530

    [Dipak] modified repeat till 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,29 +13,34 @@ namespace SnakeAndLadder
             int startPosition = 0;
             const int noPlay= 1, ladder = 2, snake = 3;
             Console.WriteLine("welcome to snake and ladder program");
+
+            while (startPosition < 100)
+            {   
+                Random random = new Random();
+                int dieValue = random.Next(1, 7);
+                Console.WriteLine("The player roll a die:" + dieValue);
             
-            Random random= new Random();
-            int dieValue = random.Next(1, 7);
-            Console.WriteLine("The player roll a die:" +dieValue);
-            
-            int option=random.Next(1, 4);
-            Console.WriteLine("option:" +option);
-            switch (option) 
-            {
-                case noPlay:
-                    Console.WriteLine("The player stays in the same position:" +startPosition);
-                    break;
-                case ladder:
-                    startPosition = startPosition + dieValue;
-                    Console.WriteLine("player got ladder and move ahead by:" +startPosition);
-                    break;
-                case snake:
-                    startPosition = startPosition - dieValue;
-                    Console.WriteLine("player got snake and move behind by:" +startPosition);
-                    break;
-                default:
-                    Console.WriteLine("invalid input");
-                    break;
+                int option = random.Next(1, 4);
+                Console.WriteLine("option:" + option);
+
+                switch (option)
+                {
+                    case noPlay:
+                        Console.WriteLine("The player stays in the same position:" + startPosition);
+                        break;
+                    case ladder:                       
+                        startPosition = startPosition + dieValue;
+                        Console.WriteLine("player got ladder and move ahead :" + startPosition);
+                        break;
+                    case snake:
+                        startPosition = startPosition - dieValue;
+                        Console.WriteLine("player got snake and move behind :" + startPosition);
+                        break;                      
+                }
+                if (startPosition < 0 )
+                {
+                    startPosition = 0;
+                }
             }
             Console.ReadLine();
         }
